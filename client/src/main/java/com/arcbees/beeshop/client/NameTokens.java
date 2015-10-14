0cd4e9cf255e43559ff91e0f3fcc9e99119757a2
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-10-14 11:22:57 -0400

    Fix PR Comments

--- client/src/main/java/com/arcbees/beeshop/client/NameTokens.java
+++ client/src/main/java/com/arcbees/beeshop/client/NameTokens.java
@@ -17,6 +17,6 @@
 package com.arcbees.beeshop.client;
 
 public class NameTokens {
-    public static final String HOME = "/home";
-    public static final String PRODUCT = "/product";
+    public static final String HOME = "!/home";
+    public static final String PRODUCT = "!/product";
 }
