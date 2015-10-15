82e35395aff52bb9132d529de9dec8ced9ad1f27
Joel Trottier-Hebert <iamquebecois@gmail.com>
2015-10-15 09:14:39 -0400

    Replacing hard-coded price for widgets

--- client/src/main/java/com/arcbees/beeshop/client/resources/AppMessages.java
+++ client/src/main/java/com/arcbees/beeshop/client/resources/AppMessages.java
@@ -5,4 +5,16 @@ import com.google.gwt.i18n.client.Messages;
 
 public interface AppMessages extends Messages {
     String brandName(@Select Brand brand);
+
+    String shirt();
+
+    String bag();
+
+    String cup();
+
+    String mug();
+
+    String key();
+
+    String phoneCase();
 }
