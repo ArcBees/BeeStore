6c6880e493139be5f9551ae1ff7a11994262ba6c
Jean-Christophe Lariviere <jclariviere@arcbees.com>
2015-10-29 10:03:01 -0400

    Code review fixes

--- client/src/main/java/com/arcbees/beeshop/client/application/widget/sidepanel/cart/CartItemsPresenter.java
+++ client/src/main/java/com/arcbees/beeshop/client/application/widget/sidepanel/cart/CartItemsPresenter.java
@@ -66,7 +66,5 @@ public class CartItemsPresenter extends PresenterWidget<CartItemsPresenter.MyVie
         } else {
             getView().showAndSetSubTotal(currentOrder.calculateSubTotal());
         }
-
-
     }
 }
